fe913e1b7cd092aa2cc7b19c8ff24687e2348005
zhouchong <zhouchong@jiae.com>
2016-04-06 13:55:49 +0800

    add AsyncStorage&屏幕宽高 assembly

---
+++ index.android - 副本AsyncStorage.js
@@ -0,0 +1,115 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+// frist app
+
+'use strict';
+import React, {
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    NetInfo,
+    AsyncStorage,
+    TouchableHighlight,
+} from 'react-native';
+var STORAGE_KEY_ONE = '@AsyncStorageDemo:key_one';
+var STORAGE_KEY_MESSAGE = '@AsyncStorageDemo:key_message';
+class CustomButton extends React.Component {
+    render() {
+        return (
+            <TouchableHighlight
+                style={styles.button}
+                underlayColor="#a5a5a5"
+                onPress={this.props.onPress}>
+                <Text style={styles.buttonText}>{this.props.text}</Text>
+            </TouchableHighlight>
+        );
+    }
+}
+class AwesomeProject extends Component {
+    // 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+           message:[]
+        };
+    }
+
+    //组件挂载之后回调方法
+    componentDidMount(){
+        this._loadInitialState().done();
+    }
+    //初始化数据-默认从AsyncStorage中获取数据
+    async _loadInitialState(){
+        try{
+            var value=await AsyncStorage.getItem(STORAGE_KEY_ONE);
+            if(value!=null){
+                this._appendMessage('从存储中获取到数据为:'+value);
+            }else{
+                this._appendMessage('存储中无数据,初始化为空数据');
+            }
+        }catch(error){
+            this._appendMessage('AsyncStorage错误'+error.message);
+        }
+    }
+    //进行储存数据_ONE
+    async _saveValue_One(){
+        try{
+            await AsyncStorage.setItem(STORAGE_KEY_ONE,'我是老王');
+            this._appendMessage('保存到存储的数据为:'+'我是老王');
+        }catch(error){
+            this._appendMessage('AsyncStorage错误'+error.message);
+        }
+    }
+    //进行存储数据删除_ONE
+    async _removeValue_One(){
+        try{
+            await AsyncStorage.removeItem(STORAGE_KEY_ONE);
+            this._appendMessage('数据删除成功...');
+        }catch(error){
+            this._appendMessage('AsyncStorage错误'+error.message);
+        }
+    }
+    //进行把message信息添加到messages数组中
+    _appendMessage(message){
+        this.setState({messages:this.state.messages.concat(message)});
+    }
+
+    render() {
+        return (
+            <View>
+                <Text style={styles.welcome}>
+                    AsyncStorage使用实例
+                </Text>
+                <CustomButton text='存储数据' onPress={this._saveValue_One}/>
+                <CustomButton text='删除数据' onPress={this._removeValue_One}/>
+                <Text style={styles.content}>信息为：</Text>
+                {this.state.message.map((m)=> <Text style={styles.content} key={m}>{m} </Text>)}
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    welcome: {
+        fontSize: 16,
+        textAlign: 'left',
+        margin: 10
+    },
+    content:{
+        fontSize: 13,
+        textAlign: 'left',
+        margin: 10,
+    },
+    button: {
+        margin:5,
+        backgroundColor: 'white',
+        padding: 15,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: '#cdcdcd',
+    }
+});
+AppRegistry.registerComponent('AwesomeProject', ()=> AwesomeProject);
